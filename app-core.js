523b836c3b7b277b57d899abba0888e8aa368960
mepeele <32821857+mepeele@users.noreply.github.com>
2026-05-26 06:11:35 -0700

    Create app-core.js

---
+++ app-core.js
@@ -0,0 +1,16 @@
+let currentStage = 'Clock In', timeIn = null, lunchOut = null, lunchIn = null, timeOut = null, weeklyLogs = [], targetHours = 8;
+
+window.onload = function() {
+    if(localStorage.getItem('weekly_logs')) weeklyLogs = JSON.parse(localStorage.getItem('weekly_logs'));
+    if(localStorage.getItem('target_hours')) targetHours = parseInt(localStorage.getItem('target_hours'));
+    if(localStorage.getItem('saved_stage')) {
+        currentStage = localStorage.getItem('saved_stage');
+        if(localStorage.getItem('saved_timeIn')) timeIn = new Date(localStorage.getItem('saved_timeIn'));
+        if(localStorage.getItem('saved_lunchOut')) lunchOut = new Date(localStorage.getItem('saved_lunchOut'));
+        if(localStorage.getItem('saved_lunchIn')) lunchIn = new Date(localStorage.getItem('saved_lunchIn'));
+    }
+    clearOldWeeksLogs(); updateUI();
+};
+
+function saveLogs() { localStorage.setItem('weekly_logs', JSON.stringify(weeklyLogs)); }
+function updateLiveUnits() { localStorage.setItem('saved_manual_units', document.getElementById('prodInput').value); }
